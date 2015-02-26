cacbbd4bb83436aebe8e7040f84549beb1185eb6
Karl Czajkowski <karlcz@github.com>
2015-02-26 13:23:44 -0800

    initial untested mapping to boto S3 client bindings

--- hatrac/model/storage/amazons3.py
+++ hatrac/model/storage/amazons3.py
@@ -10,9 +10,66 @@ This module handles only low-level byte storage. Object and
 object-version lifecycle and authorization is handled by the caller.
 
 """
+from webauthn2.util import PooledConnection
+import boto
+import boto.s3
+import boto.s3.key
 from hatrac.core import BadRequest, coalesce
 
-class HatracStorage (object):
+
+class PooledS3Connection (object):
+
+    def __init__(self, config):
+        """Represent a pool of S3 connections and bucket handles.
+
+           Config is a web.storage object with keys:
+              s3_bucket: name of bucket
+
+              s3_connection: nested dictionary of parameters passed as
+                 boto.S3Connection(**dict(s3_connection))
+
+        """
+        self.bucket_name = config['s3_bucket']
+
+        # TODO: do something better for authz than storing secret in our config data!
+        self.conn_config = config['s3_connection']
+
+        # TODO: encode better identifiers if we ever support multiple
+        # buckets or credentials and need separate sub-pools
+        self.config_tuple = (self.bucket_name,)
+
+    def _new_connection(self):
+        """Open a new S3 connection and get bucket handle."""
+        conn = boto.S3Connection(**self.conn_config)
+        bucket = conn.get_bucket(self.bucket_name)
+        return (conn, bucket)
+
+def s3_bucket_wrap(deferrered_conn_reuse=False):
+    """Decorate a method with pooled bucket access.
+
+       If deferred_conn_reuse is True, wrapped method must put back
+       (conn, bucket) pair into pool itself, e.g. at end of lazy data
+       generator.
+    """
+    def decorator(orig_method):
+        def wrapper(*args):
+            self = args[0]
+            conn_tuple = None
+            try:
+                conn_tuple = self._get_pooled_connection()
+                conn, bucket = conn_tuple
+                try:
+                    return orig_method(*args, s3_conn=conn, s3_bucket=bucket)
+                except Exception, ev:
+                    # TODO: catch and map S3 exceptions into hatrac.core.* exceptions?
+                    conn_tuple = None
+            finally:
+                if conn_tuple and not deferred_conn_reuse:
+                    self._put_pooled_connection(conn_tuple)
+        return wrapper
+    return decorator
+
+class HatracStorage (PooledS3BucketConnection):
     """Implement HatracStorage API using an S3 bucket.
 
        A configured storage bucket, object name, and object version
@@ -31,228 +88,95 @@ class HatracStorage (object):
     # Hatrac versions directly in S3 object names (seperate S3 object
     # per Hatrac object-version)...  perhaps a config option?
 
-    # TODO: generally raise hatrac.core.* exceptions when things go wrong
-
-    # BUG: hatrac doesn't really expect many storage exceptions and
-    # might need new handling to pass transient errors back to client
-    # in a way that allows client-driven compensation
-
     _bufsize = 1024**2
 
     def __init__(self, config):
-        self.bucket = config['s3_bucket']
-        # TODO: define and get any other config parameters required to talk to S3
+        """Represents an Hatrac storage interface backed by an S3 bucket.
 
-    def create_from_file(self, name, input, nbytes, content_type=None, content_md5=None):
+           See PooledS3Connection for config content documentation.
+        """
+        PooledS3BucketConnection.__init__(self, config)
+
+    @s3_bucket_wrap()
+    def create_from_file(self, name, input, nbytes, content_type=None, content_md5=None, s3_conn=None, s3_bucket=None):
         """Create an entire file-version object from input content, returning version ID."""
-        
-        raise NotImplementedError()
+        s3_key = boto.s3.key.Key(s3_bucket, name)
+        headers = {'Content-Length': str(nbytes)}
+        if content_type is not None:
+            headers['Content-Type'] = content_type
+        if content_md5 is not None:
+            headers['Content-MD5'] = content_md5
+        s3_key.set_contents_from_stream(input, headers=headers, replace=True, md5=md5, size=nbytes)
+        return s3_key.version_id
 
-        REQUEST = """
-PUT /%(name)s
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-Content-Type: %(content_type)s
-Content-Length: %(nbytes)d
-Content-MD5: %(content_md5)s
-
-...entity...
-"""
-        # TODO: read nbytes from input and stream into request as request entity
-
-        RESPONSE = """
-HTTP/1.1 200 OK
-...
-X-amz-id-version-id: ...version_id...
-...
-"""
-        # TODO: extract version_id payload from response
-        return version_id
-
-    def create_upload(self, name, nbytes=None, content_type=None, content_md5=None):
-
-        raise NotImplementedError()
-
-        REQUEST = """
-POST /%(name)s?uploads
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-Content-Type: %(content_type)s
-"""
-
-        RESPONSE = """
-HTTP/1.1 200 OK
-Content-Length: N
-
-<?xml version="1.0" encoding="UTF-8"?>
-<InitiateMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-  <Bucket>...bucket...</Bucket>
-  <Key>...object name...</Key>
-  <UploadId>...upload_id...</UploadId>
-</InitiateMultipartUploadResult>
-"""
-        # TODO: extract UploadId element body from response XML
-        return upload_id
-
-    def upload_chunk_from_file(self, name, upload_id, position, chunksize, input, nbytes, content_md5=None, f=None):
-
-        raise NotImplementedError()
-
-        REQUEST = """
-PUT /%(name)s?partNumber=%(position)d&uploadId=%(upload_id)s HTTP/1.1
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-Content-Length: %(nbytes)d
-Content-MD5: %(content_md5)s
-
-...entity...
-"""
-        # TODO: read nbytes from input and stream into request as request entity
-
-        RESPONSE = """
-HTTP/1.1 200 OK
-...
-ETag: "...ETag value..."
-...
-"""
-        # TODO: extract Etag value from chunk response
-
-        # return auxilliary chunk info for directory layer to track for us
-        return dict(etag=etag_value)
-
-               
     def get_content(self, name, version, content_md5=None):
-
-        raise NotImplementedError()
-
-        REQUEST = """
-GET /%(name)s?versionId=%(version)s
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-"""
-
-        RESPONSE = """
-HTTP/1.1 200 OK
-Content-Length: ...nbytes...
-Content-Type: ...content_type...
-Content-MD5: ...content_md5...
-
-...response entity...
-"""
-        # TODO extract response headers and construct a
-        # data-generating closure that will yield response entity of nbytes length
-        # in efficiently buffered reads
-
-        return nbytes, content_type, content_md5, data_generator
+        return self.get_content_range(name, version, content_md5, None)
      
-    def get_content_range(self, name, version, content_md5=None, get_slice=None):
+    @s3_bucket_wrap(deferred_conn_reuse=True)
+    def get_content_range(self, name, version, content_md5=None, get_slice=None, s3_conn=None, s3_bucket=None):
+        s3_key = boto.s3.key.Key(s3_bucket, name)
+        s3_key.version_id = version
 
-        raise NotImplementedError()
+        if get_slice:
+            first = get_slice.start
+            last = get_slice.stop - 1
+            headers = {'Range':'bytes=%d-%d' % (first, last)}
+            rbytes = last - first + 1
+        else:
+            headers = None
+            rbytes = None
 
-        # TODO: unpack Range first/last if get_slice is not None
-        # otherwise behave same as self.get_content() above
+        s3_key.open_read(headers=headers)
 
-        # (convert from Python slice indexing to HTTP Range indexing
-        # which uses inclusive limit)
-        first = get_slice.start
-        last = get_slice.stop - 1
+        if rbytes is None:
+            rbytes = s3_key.size
 
-        REQUEST = """
-GET /%(name)s?versionId=%(version)s
-Host: %(bucket)s.s3.amazonaws.com
-Range: bytes=%(first)d-%(last)d
-Authorization: %(s3_authz)s
-"""
+        def data_generator():
+            conn_tuple = (s3_conn, s3_bucket)
+            try:
+                for bytes in s3_key:
+                    yield bytes
+            except Exception, ev:
+                # discard connections which had errors?
+                conn_tuple = None
+            finally:
+                if conn_tuple:
+                    self._put_pooled_connection(conn_tuple)
 
-        RESPONSE = """
-HTTP/1.1 206 Partial Content
-Content-Length: ...nbytes...
-Content-Type: ...content_type...
-Content-Range: bytes first-last/total
-Content-MD5: ...content_md5...
+        return rbytes, s3_key.content_type, s3_key.md5, data_generator()
 
-...response entity...
-"""
-        # TODO extract response headers and construct a
-        # data-generating closure that will yield response entity of nbytes length
-        # in efficiently buffered reads
-
-        # note nbytes is number of bytes in partial read, not total bytes
-        return nbytes, content_type, content_md5, data_generator
-
-
-    def cancel_upload(self, upload_id):
-
-        raise NotImplementedError()
-
-        REQUEST = """
-DELETE /%(name)s?uploadId=%(upload_id)s HTTP/1.1
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-"""
-
-        RESPONSE = """
-HTTP/1.1 204 OK
-...
-"""
-
-
-    def finalize_upload(self, name, upload_id, chunk_data):
-
-        raise NotImplementedError()
-
-        # TODO: build request XML entity using chunk_data list
-
-        chunk.sort(key=lambda c: c.position)
-
-        for chunk in chunk_data:
-            assert chunk.position is not None
-            assert chunk.etag is not None
-
-        REQUEST = """
-POST /%(name)s?uploadId=%(upload_id)s HTTP/1.1
-Host: %(bucket)s.s3.amazonaws.com
-Authorization: %(s3_authz)s
-
-<CompleteMultipartUpload>
-  <Part>
-    <PartNumber>...part number...</PartNumber>
-    <ETag>ETag</ETag>
-  </Part>
-  ...
-</CompleteMultipartUpload>
-"""
-
-        RESPOSE = """
-HTTP/1.1 200 OK
-...
-X-amz-version-id: ...version_id...
-
-<?xml version="1.0" encoding="UTF-8"?>
-<CompleteMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-  <Location>...object URL...</Location>
-  <Bucket>...bucket...</Bucket>
-  <Key>...name...</Key>
-  <ETag>...ETag...</ETag>
-</CompleteMultipartUploadResult>
-"""
-        # TODO: extract version_id from response header
-        return version_id
-
-    def delete(self, name, version):
+    @s3_bucket_wrap()
+    def delete(self, name, version, s3_conn=None, s3_bucket=None):
         """Delete object version."""
-        
-        raise NotImplementedError()
+        s3_key = boto.s3.key.Key(s3_bucket, name)
+        s3_key.version_id = version
+        s3_key.delete()
 
-        REQUEST = """
-DELETE /%(name)s?versionId=%(version)s
-Host: %(bucket)s.s3.amazonaws.com
-"""
+    @s3_bucket_wrap()
+    def create_upload(self, name, nbytes=None, content_type=None, content_md5=None, s3_conn=None, s3_bucket=None):
+        upload = s3_bucket.initiate_multipart_upload(name)
+        return upload.id
 
-        RESPONSE = """
-HTTP/1.1 204 No Content
-"""
-        return None
+    @s3_bucket_wrap()
+    def upload_chunk_from_file(self, name, upload_id, position, chunksize, input, nbytes, content_md5=None, s3_conn=None, s3_bucket=None):
+        upload = boto.s3.multipart.MultiPartUpload(s3_bucket)
+        upload.id = upload_id
+        s3_key = upload.upload_part_from_file(input, position, md5=(content_md5, content_md5_b64), size=nbytes)
+        return dict(etag=s3_key.etag)
+               
+    @s3_bucket_wrap()
+    def cancel_upload(self, upload_id, s3_conn=None, s3_bucket=None):
+        upload = boto.s3.multipart.MultiPartUpload(s3_bucket)
+        upload.id = upload_id
+        upload.cancel_upload()
+
+    @s3_bucket_wrap()
+    def finalize_upload(self, name, upload_id, chunk_data, s3_conn=None, s3_bucket=None):
+        upload = boto.s3.multipart.MultiPartUpload(s3_bucket)
+        upload.id = upload_id
+        # TODO: is chunk_data even necessary...?
+        upload = upload.complete_upload()
+        return upload.version_id
 
     def delete_namespace(self, name):
         """Tidy up after an empty namespace that has been deleted."""
